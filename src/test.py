868a131205916630e13c546391de8819c22de427
Daniel McPartland <dan.mcp@gmail.com>
2022-04-21 15:44:38 -0500

    fix: good typing

--- src/test.py
+++ src/test.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     print(os.environ)
     for name, value in os.environ.items():
         if "github" in name.lower():
-            print(f"{name}:{value})"
+            print(f"{name}:{value}")
