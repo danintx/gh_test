849f2400a68cf2c8aaf67a597ca31fc3ba39d195
Daniel McPartland <dan.mcp@gmail.com>
2022-04-22 12:51:00 -0500

    more changes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="gh_test",
+    version="0.0dev1",
+    packages=["gh_test"],
+    url="http://notreal",
+    license="",
+    author="danmc",
+    author_email="dan.mcp@gmail.com",
+    description="fill out later",
+    install_requires=["PyGithub", "requests"],
+)
